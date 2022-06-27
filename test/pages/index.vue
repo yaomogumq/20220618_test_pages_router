0fe322b22d030255c2b19a3a4105a62c2bd85ba8
Yaomogumq <yaomogumq@gmail.com>
2022-06-27 13:33:44 +0800

    treeview added

--- test/pages/index.vue
+++ test/pages/index.vue
@@ -2,10 +2,11 @@
   <div class="app">
     
     <sidebar/>
-    <div>
+    <div class="content">
     <h1>hi</h1>
     <button><a class="text-gray-800" :href="signin_url"> Click me to sign in </a> </button>
-	 <pre>{{hubs}}hihi</pre>
+	<p><TreeItem class="item" :model="treeData"></TreeItem></p>
+	<pre>{{hubs}}</pre>
     </div>
     
     
@@ -25,6 +26,14 @@
 	--dark-alt: #bbd8bf;
 	--light: #f1f5f9;
 	--sidebar-width: 300px;
+}
+.content {
+	flex: 1 1 0;
+}
+.item {
+	padding-left: 6rem;
+	color: var(--primary-alt);
+
 }
 * {
 	margin: 0;
@@ -58,12 +67,16 @@ button {
 <script setup>
 import Sidebar from '~/components/Sidebar';
 import ForgeSDK from 'forge-apis';
+import {ref} from 'vue';
+import TreeItem from '~/components/TreeItem';
+
+
+
 
 var HubsApi = new ForgeSDK.HubsApi(); //Hubs Client
-
 var url = new URL(document.URL);
 var authorizationCode = 'null';
-var FORGE_CLIENT_ID = 'ovVGST7XyRDWeIyGUdYCmgtZF6IvVYZ4', FORGE_CLIENT_SECRET = 'T8ec853f398e', REDIRECT_URL = 'http://localhost:3001';
+var FORGE_CLIENT_ID = 'ovVGST7XyRDWeIyGUdYCmgtZF6IvVYZ4', FORGE_CLIENT_SECRET = 'T8ec853f398e', REDIRECT_URL = 'http://localhost:3000';
 // Initialize the 3-legged OAuth2 client, set specific scopes and optionally set the `autoRefresh` parameter to true
 // if you want the token to auto refresh
 var scope='data:read';
@@ -74,24 +87,32 @@ var oAuth2ThreeLegged = new ForgeSDK.AuthClientThreeLegged(FORGE_CLIENT_ID, FORG
 var credentials={
   access_token:''
 }
-
 let signin_url = `https://developer.api.autodesk.com/authentication/v1/authorize?response_type=token&client_id=${FORGE_CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${scope}`;
-let hubs;
+let hubs = ref(null);
 //console.log(signin_url);
+
+
+
+
  try{
   authorizationCode=url.hash;
   credentials.access_token = authorizationCode.replace(/(#access_token=)|&[\s\S]+/g,'');
   hubs= await HubsApi.getHubs({}, oAuth2ThreeLegged, credentials).then((hubs) => 
 
-	 JSON.stringify(hubs.body.data)).then((body) => { return JSON.parse(body);})
+	 JSON.stringify(hubs.body.data[0].attributes)).then((body) => { return JSON.parse(body);})
+	 
+	//  return treeData;
     //hubs.body.data[1].attributes.name
+	
  }
-  
+
  catch(err){
  	authorizationCode='error';
  }
 
-
-console.log(hubs);
+const treeData = ref(hubs);
+//return hubs;
 
 </script>
+
+
